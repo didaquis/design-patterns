7b94d10fade7c9198cc9544c4e7fffff8d21de9d
Dídac García <didaquis@gmail.com>
2021-02-13 01:06:41 +0100

    Code has been refactored

--- src/FactoryMethod/index.ts
+++ src/FactoryMethod/index.ts
@@ -10,17 +10,21 @@ import PaymentMethodFactory from './Factory/PaymentMethodFactory';
 
 export default class Order {
 	public paymentType: PaymentMethodInterface;
-	public commission = 0;
+	private commissionApplied = 0;
 
-	//constructor(private type: PaymentType, public amount: number) {}
 	constructor(private type: PaymentType, public amount: number) {
-		// set payment type
+		// Establece el tipo de pago (Obtenemos la clase mediante inyección de dependencia)
 		this.paymentType = PaymentMethodFactory.getInstanceOfPaymentType(this.type);
 
 		this.calculateCommission();
 	}
 
 	private calculateCommission(): void {
-		this.commission = this.paymentType.comission * this.amount;
+		// Dado que todos los métodos de pago implementan la misma interfaz, podemos hacer este tipo de lógica sin saber que método de pago se va a utilizar. Todos los métodos de pago nos retornan un valor de comissión.
+		this.commissionApplied = this.paymentType.comission * this.amount;
+	}
+
+	public get commission(): number {
+		return this.commissionApplied;
 	}
 }
