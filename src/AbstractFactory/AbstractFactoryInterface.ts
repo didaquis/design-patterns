dd3d3c212e951a82aad71610522873d23ff8d47b
Dídac García <didac.garcia@promocionesfarma.com>
2023-02-15 18:06:10 +0100

    Added description

--- src/AbstractFactory/AbstractFactoryInterface.ts
+++ src/AbstractFactory/AbstractFactoryInterface.ts
@@ -3,6 +3,8 @@ import PublisherInterface from './Factories/PublisherInterface';
 
 /**
  * Todas las factorías deberan implementar estas interfaces. Esto permitirá que las factorías sean intercambiables entre ellas.
+ *
+ * Si te fijas, esta interfaz es una fábrica de fábricas. También puedes verlo como una clase que permite crear familias de clases.
  */
 
 export default interface AbstractFactoryInterface {
